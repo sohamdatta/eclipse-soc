6aff8de5c27919b3048b8cc825ff554077b396cf
karsten.knese@web.de <karsten.knese@web.de@3daf3c0a-e7dd-11dd-9ee4-63b70bd76fba>
2009-06-20 00:16:09 +0000

    Plugin with two necessary extensions - ModelRenderes & DataSetProcessor

--- trunk/2009-newBirtCharts/trunk/plugins/org.eclipse.birt.newcharts/src/org/eclipse/birt/chart/newtype/DonutSeries.java
+++ trunk/2009-newBirtCharts/trunk/plugins/org.eclipse.birt.newcharts/src/org/eclipse/birt/chart/newtype/DonutSeries.java
@@ -4,4 +4,10 @@ import org.eclipse.birt.chart.model.component.Series;
 
 public interface DonutSeries extends Series{
 
+	
+	public void setText(String text);
+	
+	public void setRadius(double rad);
+	public double getRadius();
+	
 }
