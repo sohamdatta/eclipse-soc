66846dd0c720af6bce5760fda1a789cee3d46a98
karsten.knese <karsten.knese@web.de>
2009-06-27 10:16:16 +0000

    base frame for donut-chart

--- 2009-newBirtCharts/trunk/plugins/org.eclipse.birt.newcharts/src/org/eclipse/birt/chart/newtype/render/Donut.java
+++ 2009-newBirtCharts/trunk/plugins/org.eclipse.birt.newcharts/src/org/eclipse/birt/chart/newtype/render/Donut.java
@@ -1,17 +1,32 @@
 package org.eclipse.birt.chart.newtype.render;
 
+import org.eclipse.birt.chart.computation.Rectangle;
 import org.eclipse.birt.chart.computation.withoutaxes.SeriesRenderingHints;
 import org.eclipse.birt.chart.device.IDeviceRenderer;
 import org.eclipse.birt.chart.device.IPrimitiveRenderer;
 import org.eclipse.birt.chart.event.ArcRenderEvent;
+import org.eclipse.birt.chart.event.EventObjectCache;
 import org.eclipse.birt.chart.event.RectangleRenderEvent;
+import org.eclipse.birt.chart.event.TextRenderEvent;
 import org.eclipse.birt.chart.event.WrappedStructureSource;
 import org.eclipse.birt.chart.exception.ChartException;
 import org.eclipse.birt.chart.model.attribute.Bounds;
+import org.eclipse.birt.chart.model.attribute.ColorDefinition;
 import org.eclipse.birt.chart.model.attribute.Fill;
+import org.eclipse.birt.chart.model.attribute.FontDefinition;
+import org.eclipse.birt.chart.model.attribute.Text;
+import org.eclipse.birt.chart.model.attribute.TextAlignment;
+import org.eclipse.birt.chart.model.attribute.impl.BoundsImpl;
+import org.eclipse.birt.chart.model.attribute.impl.ColorDefinitionImpl;
+import org.eclipse.birt.chart.model.attribute.impl.FontDefinitionImpl;
+import org.eclipse.birt.chart.model.attribute.impl.TextAlignmentImpl;
+import org.eclipse.birt.chart.model.attribute.impl.TextImpl;
+import org.eclipse.birt.chart.model.component.Label;
+import org.eclipse.birt.chart.model.component.impl.LabelImpl;
 import org.eclipse.birt.chart.model.layout.Legend;
 import org.eclipse.birt.chart.model.layout.Plot;
 import org.eclipse.birt.chart.model.type.PieSeries;
+import org.eclipse.birt.chart.newtype.DonutSeries;
 import org.eclipse.birt.chart.render.BaseRenderer;
 import org.eclipse.birt.chart.render.ISeriesRenderingHints;
 
@@ -38,20 +53,25 @@ public class Donut extends BaseRenderer {
 	@Override
 	public void renderSeries(IPrimitiveRenderer ipr, Plot p,
 			ISeriesRenderingHints isrh) throws ChartException {
-		
-		final SeriesRenderingHints srh = (SeriesRenderingHints) isrh;
 
-		final Bounds boCB = getCellBounds();
 		IDeviceRenderer idr = getDevice();
-		final PieSeries ps = (PieSeries) getSeries( );
-		
-		double x = boCB.getWidth() / 2;
-		double y = boCB.getHeight() / 2;
+		final DonutSeries ps = (DonutSeries) getSeries();
 
-		ArcRenderEvent are = new ArcRenderEvent( WrappedStructureSource.createSeriesDataPoint( ps,
-				srh.getDataPoints()[0].getVirtualCopy( ) ) );
-		idr.fillArc(are);
+		Label laText = LabelImpl.create();
+		Text txt = TextImpl.create(ps.getText());
+		txt.setColor(ColorDefinitionImpl.create(255, 0, 0));
+		txt.setFont(FontDefinitionImpl.create("Arial", 15, false, false, false,
+				false, false, 0.0, TextAlignmentImpl.create()));
+		laText.setCaption(txt);
 
+		TextRenderEvent tre = (TextRenderEvent) ((EventObjectCache) idr)
+				.getEventObject(WrappedStructureSource.createSeriesTitle(ps,
+						laText), TextRenderEvent.class);
+
+		tre.setLabel(laText);
+		tre.setBlockBounds(BoundsImpl.create(50, 0, 100, 100));
+		tre.setBlockAlignment(null);
+		tre.setAction(TextRenderEvent.RENDER_TEXT_IN_BLOCK);
+		idr.drawText(tre);
 	}
-
 }
