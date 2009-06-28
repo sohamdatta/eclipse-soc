8f177c27fbf78b132549802a405050c328b95d2c
higerinbeijing <higerinbeijing@3daf3c0a-e7dd-11dd-9ee4-63b70bd76fba>
2009-06-28 16:03:19 +0000

    Some lines to test row spanning.

--- trunk/2009-nebulawidgetsenhancement/Grid_Enhancement/src/GridSnippet2.java
+++ trunk/2009-nebulawidgetsenhancement/Grid_Enhancement/src/GridSnippet2.java
@@ -24,21 +24,42 @@ public static void main (String [] args) {
 
     Grid grid = new Grid(shell,SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
     grid.setHeaderVisible(true);
+    grid.setUseExtensibleSpanMethod(true);//new test code added
+    //grid.setCellSelectionEnabled(true);
+    grid.setRowHeaderVisible(true);
+    //grid.setRowsResizeable(true);
+    grid.setDragDetect(true);
+    grid.setFooterVisible(true);
     GridColumn column = new GridColumn(grid,SWT.NONE);
     column.setText("Column 1");
     column.setWidth(100);
+    column.setMoveable(true);
     GridColumn column2 = new GridColumn(grid,SWT.NONE);
     column2.setText("Column 2");
     column2.setWidth(100);
+    column2.setMoveable(true);
+    GridColumn column3 = new GridColumn(grid,SWT.NONE);
+    column3.setText("Column 3");
+    column3.setWidth(100);
+    GridColumn column4 = new GridColumn(grid,SWT.NONE);
+    column4.setText("Column 4");
+    column4.setWidth(100);
+    GridColumn column5 = new GridColumn(grid,SWT.NONE);
+    column5.setText("Column 5");
+    column5.setWidth(100);
     GridItem item1 = new GridItem(grid,SWT.NONE);
     item1.setText("First Item");
     item1.setText(1,"xxxxxxx");
     GridItem item2 = new GridItem(grid,SWT.NONE);
-    item2.setText("This cell spans both columns");
+    item2.setText(0,"This cell spans both columns");
     item1.setText(1,"xxxxxxx");
-    item2.setColumnSpan(0,1);
+//    item2.setColumnSpan(0,1);
+//    item2.setColumnSpan(3, 1);
+    item2.setAreaSpan(0, 1, 1);//new added test code
+    item2.setAreaSpan(3, 1, 1);
     GridItem item3 = new GridItem(grid,SWT.NONE);
     item3.setText("Third Item");
+    //item3.setAreaSpan(3, 1, 0);
     item1.setText(1,"xxxxxxx");
     
     shell.setSize(200,200);
