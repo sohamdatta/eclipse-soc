3c252fc27410fed648b8e43d8a201ca369d83e31
mateusz.matela <mateusz.matela@3daf3c0a-e7dd-11dd-9ee4-63b70bd76fba>
2009-07-27 17:57:28 +0000

    renamed package names back to original names for simplicity

---
+++ trunk/2009-treeviewsforzest/widgets/src/org/eclipse/zest/core/widgets/InternalNodeLayout.java
@@ -0,0 +1,202 @@
+package org.eclipse.zest.core.widgets;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.zest.layout.dataStructures.DisplayIndependentDimension;
+import org.eclipse.zest.layout.dataStructures.DisplayIndependentPoint;
+import org.eclipse.zest.layout.interfaces.ConnectionLayout;
+import org.eclipse.zest.layout.interfaces.NodeLayout;
+import org.eclipse.zest.layout.interfaces.SubgraphLayout;
+
+class InternalNodeLayout implements NodeLayout {
+	private DisplayIndependentPoint location;
+	private DisplayIndependentDimension size;
+	private boolean minimized = false;
+	private final GraphNode node;
+	private final InternalLayoutContext ownerLayoutContext;
+	private SubgraphLayout subgraph;
+
+	public InternalNodeLayout(GraphNode graphNode) {
+		this.node = graphNode;
+		this.ownerLayoutContext = node.parent.getLayoutContext();
+	}
+
+	public DisplayIndependentPoint getLocation() {
+		if (location == null) {
+			refreshLocation();
+		}
+		return new DisplayIndependentPoint(location);
+	}
+
+	public DisplayIndependentDimension getSize() {
+		if (size == null) {
+			refreshSize();
+		}
+		return new DisplayIndependentDimension(size);
+	}
+
+	public SubgraphLayout getSubgraph() {
+		return subgraph;
+	}
+
+	public boolean isMovable() {
+		return true;
+	}
+
+	public boolean isPrunable() {
+		return ownerLayoutContext.isPruningEnabled();
+	}
+
+	public boolean isPruned() {
+		return subgraph != null;
+	}
+
+	public boolean isResizable() {
+		return (node.parent.getAdaptee().getStyle() & ZestStyles.NODES_NO_LAYOUT_RESIZE) == 0;
+	}
+
+	public void prune(SubgraphLayout subgraph) {
+		if (subgraph == this.subgraph)
+			return;
+		if (this.subgraph != null) {
+			SubgraphLayout subgraph2 = this.subgraph;
+			this.subgraph = null;
+			subgraph2.removeNodes(new NodeLayout[] { this });
+		}
+		if (subgraph != null) {
+			this.subgraph = subgraph;
+			subgraph.addNodes(new NodeLayout[] { this });
+		}
+	}
+
+	public void setLocation(double x, double y) {
+		if (location != null) {
+			location.x = x;
+			location.y = y;
+		} else {
+			location = new DisplayIndependentPoint(x, y);
+		}
+	}
+
+	public void setSize(double width, double height) {
+		if (size != null) {
+			size.width = width;
+			size.height = height;
+		} else {
+			size = new DisplayIndependentDimension(width, height);
+		}
+	}
+
+	public void setMinimized(boolean minimized) {
+		getSize();
+		this.minimized = minimized;
+	}
+
+	public boolean isMinimized() {
+		return minimized;
+	}
+
+	public NodeLayout[] getPredecessingNodes() {
+		ConnectionLayout[] connections = getIncomingConnections();
+		NodeLayout[] result = new NodeLayout[connections.length];
+		for (int i = 0; i < connections.length; i++) {
+			result[i] = connections[i].getSource();
+			if (result[i] == this)
+				result[i] = connections[i].getTarget();
+		}
+		return result;
+	}
+
+	public NodeLayout[] getSuccessingNodes() {
+		ConnectionLayout[] connections = getOutgoingConnections();
+		NodeLayout[] result = new NodeLayout[connections.length];
+		for (int i = 0; i < connections.length; i++) {
+			result[i] = connections[i].getTarget();
+			if (result[i] == this)
+				result[i] = connections[i].getSource();
+		}
+		return result;
+	}
+
+	public NodeLayout[] getSuccessingEntities() {
+		// TODO Auto-generated method stub
+		return getSuccessingNodes();
+	}
+
+	public NodeLayout[] getPredecessingEntities() {
+		// TODO Auto-generated method stub
+		return getPredecessingNodes();
+	}
+
+	public ConnectionLayout[] getIncomingConnections() {
+		ArrayList result = new ArrayList();
+		for (Iterator iterator = node.getTargetConnections().iterator(); iterator.hasNext();) {
+			GraphConnection connection = (GraphConnection) iterator.next();
+			if (!ownerLayoutContext.isLayoutItemFiltered(connection))
+				result.add(connection.getLayout());
+		}
+		for (Iterator iterator = node.getSourceConnections().iterator(); iterator.hasNext();) {
+			GraphConnection connection = (GraphConnection) iterator.next();
+			if (!connection.isDirected() && !ownerLayoutContext.isLayoutItemFiltered(connection))
+				result.add(connection.getLayout());
+		}
+		return (ConnectionLayout[]) result.toArray(new ConnectionLayout[result.size()]);
+	}
+
+	public ConnectionLayout[] getOutgoingConnections() {
+		ArrayList result = new ArrayList();
+		for (Iterator iterator = node.getSourceConnections().iterator(); iterator.hasNext();) {
+			GraphConnection connection = (GraphConnection) iterator.next();
+			if (!ownerLayoutContext.isLayoutItemFiltered(connection))
+				result.add(connection.getLayout());
+		}
+		for (Iterator iterator = node.getTargetConnections().iterator(); iterator.hasNext();) {
+			GraphConnection connection = (GraphConnection) iterator.next();
+			if (!connection.isDirected() && !ownerLayoutContext.isLayoutItemFiltered(connection))
+				result.add(connection.getLayout());
+		}
+		return (ConnectionLayout[]) result.toArray(new ConnectionLayout[result.size()]);
+	}
+
+	public double getPreferredAspectRatio() {
+		return 0;
+	}
+
+	void applyLayout() {
+		if (minimized) {
+			node.setSize(0, 0);
+			if (location != null)
+				node.setLocation(location.x, location.y);
+		} else {
+			node.setSize(-1, -1);
+			if (location != null)
+				node.setLocation(location.x - getSize().width / 2, location.y - size.height / 2);
+			if (size != null) {
+				Dimension currentSize = node.getSize();
+				if (size.width != currentSize.width || size.height != currentSize.height)
+					node.setSize(size.width, size.height);
+			}
+		}
+	}
+
+	InternalLayoutContext getOwnerLayoutContext() {
+		return ownerLayoutContext;
+	}
+
+	void refreshSize() {
+		Dimension size2 = node.getSize();
+		setSize(size2.width, size2.height);
+	}
+
+	void refreshLocation() {
+		Point location2 = node.getLocation();
+		setLocation(location2.x + getSize().width / 2, location2.y + size.height / 2);
+	}
+
+	public String toString() {
+		return node.toString() + "(layout)";
+	}
+}
---
+++ trunk/2009-treeviewsforzest/widgets/src/org/eclipse/zest/core/widgets/LabelSubgraphLayout.java
@@ -0,0 +1,80 @@
+package org.eclipse.zest.core.widgets;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.Label;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.zest.core.widgets.internal.GraphLabel;
+import org.eclipse.zest.layout.interfaces.NodeLayout;
+import org.eclipse.zest.layout.interfaces.SubgraphLayout;
+
+/**
+ * A subgraph layout that displays a label showing number of items pruned within
+ * it.
+ */
+public class LabelSubgraphLayout extends FigureSubgraphLayout {
+
+	public final static SubgraphFactory FACTORY = new SubgraphFactory() {
+		public SubgraphLayout createSubgraph(NodeLayout[] nodes, InternalLayoutContext context) {
+			return new LabelSubgraphLayout(nodes, context);
+		}
+	};
+
+	private static Color defaultForegroundColor = ColorConstants.black;
+	private static Color defaultBackgroundColor = ColorConstants.yellow;
+
+	/**
+	 * Changes the default foreground color for newly created subgraphs.
+	 * 
+	 * @param c
+	 *            color to use
+	 */
+	public static void setDefualtForegroundColor(Color c) {
+		defaultForegroundColor = c;
+	}
+
+	/**
+	 * Changes the default background color for newly created subgraphs.
+	 * 
+	 * @param c
+	 *            color to use
+	 */
+	public static void setDefaultBackgroundColor(Color c) {
+		defaultBackgroundColor = c;
+	}
+
+	/**
+	 * Sets the foreground color of this subgraph (that is color of the text on
+	 * the label).
+	 * 
+	 * @param color
+	 *            color to set
+	 */
+	public void setForegroundColor(Color color) {
+		figure.setForegroundColor(color);
+	}
+
+	/**
+	 * Sets the background color of this subgraph's label.
+	 * 
+	 * @param color
+	 *            color to set
+	 */
+	public void setBackgroundColor(Color color) {
+		figure.setBackgroundColor(color);
+	}
+
+	protected void createFigure() {
+		figure = new GraphLabel(false);
+		figure.setForegroundColor(defaultForegroundColor);
+		figure.setBackgroundColor(defaultBackgroundColor);
+		updateFigure();
+	}
+
+	protected void updateFigure() {
+		((Label) figure).setText("" + nodes.size());
+	}
+
+	protected LabelSubgraphLayout(NodeLayout[] nodes, InternalLayoutContext context) {
+		super(nodes, context);
+	}
+}
